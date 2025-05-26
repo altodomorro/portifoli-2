f8bd7dbaba6b83263cd67ac4eb8adc8bdffa629b
altodomorro <lino.vinicius@escola.pr.gov.br>
2025-05-26 15:24:39 +0000

    ATIVIDADE 2

---
+++ ex2/script2.js
@@ -0,0 +1,19 @@
+Function soma(){
+let N1=0;
+let N2=0;
+let soma=0;
+
+
+N1=Number(window.prompt("DIGITE UM NUMERO:"));
+
+soma=2025-N1
+N2=10-soma
+
+
+if(SOMA>10){}
+        alert("VOCE JA PODE TIRAR A ACERTEIRA, VOCE TEM MAIS DE 18 ANOS");
+
+}
+else{
+alert("AINDA NAO E MAIOR DE IDADE,FALATS: " + N2 +"ANO(S) PARA TIRAR A CARTEIRA DE MOTPRISTA")
+}
--- exercico 1/script1.js
+++ exercico 1/script1.js
@@ -5,4 +5,17 @@ let num2=0;
 
 num1=number(window.prompt("digite um numero:"));
 num2=number(window.prompt("digite um numero:"));
+//
+letsoma =num1+num2;
 
+alert(somo)
+
+
+
+if(soma>10) 
+        alert("o resultado e maior que 10")
+
+
+if (soma<10)
+alert9"o resultado e menor que 10")
+}
