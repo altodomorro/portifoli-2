86de122fecfa348314d0a17ae8b2ac20874fde7b
altodomorro <lino.vinicius@escola.pr.gov.br>
2025-05-26 11:56:31 +0000

    atividade

---
+++ ex4/script4.js
@@ -0,0 +1,5 @@
+function media()
+let nota1= number (window.prompt("digite a primeira nota"));
+let nota2= number (window.prompt("digite a segunda nota"));
+
+
---
+++ exercico 1/script1.js
@@ -0,0 +1,8 @@
+function somar(){
+let num1=0;
+let num2=0;
+
+
+num1=number(window.prompt("digite um numero:"));
+num2=number(window.prompt("digite um numero:"));
+
