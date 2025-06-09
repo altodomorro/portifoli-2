685e373186356d5db0dac09cccf208f6098647d8
altodomorro <lino.vinicius@escola.pr.gov.br>
2025-06-09 15:24:15 +0000

    continuação 3

---
+++ ex3/script3.js
@@ -0,0 +1,29 @@
+//cauculadora simples com 5 operaçoes basicas;
+//
+
+
+
+
+
+
+function somar(){
+let num1=0;
+let num2=0;
+
+
+num1=number(window,prompt('digite um numero;'));
+num2=number(window,prompt('digite um numero;'));
+
+let soma =num1+num2;
+
+function subtração (){
+
+    num3=num1-num2
+
+
+    num1-number(window.prompt(digite um numero;));
+
+    num2-number(window.prompt(digite um numero;));
+}
+
+    num3=num1-num2
--- ex4/script4.js
+++ ex4/script4.js
@@ -1,8 +1,20 @@
-function media()
+function media() {
 let nota1= number (window.prompt("digite a primeira nota"));
 let nota2= number (window.prompt("digite a segunda nota"));
-let nota3= number (window.prompt("digite a segunda nota"));
+let nota3= number (window.prompt("digite a primeira nota"));
 
 let media =( nota1+nota2+nota) /3;
 
-let resultado = document.quarySelector('
+let resultado = document.quarySelector('#resultado');
+resultado.innerHTML = '<p>o resultado é $(media)</p>';
+
+console.log(resultado);
+
+}
+
+if (media <6) {
+
+    let med =6 -media
+    resultado.innerHTML= '<p> reprovado </p>
+}
+
